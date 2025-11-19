627efbba3ed099215f1e643a86e45dcc54f4c3fe
fredy bonilla rey <fredyto14br@gmail.com>
2025-11-19 10:12:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7110644053-ea023",
+  "appId": "1:627441336734:web:2faf265ce1e9aed2c2968f",
+  "apiKey": "AIzaSyBd7nuPFXbDcvKEZtC8sDcuDm0x2OCYzts",
+  "authDomain": "studio-7110644053-ea023.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "627441336734"
+};
